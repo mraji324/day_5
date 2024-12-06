40d2668bd1f0b6f1192e39a23bf542e252c62b98
Hindu <rajuhindur@gmail.com>
2024-12-06 14:45:25 +0530

    Add files via upload

---
+++ Odd2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Odd2
+{
+    public static void main(String[] args)
+    {
+        int n=1;
+        while(n<=100)
+        {
+            if(n%2!=0)
+            System.out.println(n);
+            n++;
+        }
+    }
+}
